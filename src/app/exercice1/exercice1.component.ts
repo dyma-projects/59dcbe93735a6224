7bf0c99768550f347780d19727fe9bb33df86eec
florian.cerda@richemont.com <florian.cerda@richemont.com>
2019-04-09 17:02:45 +0200

    exercice1

--- src/app/exercice1/exercice1.component.ts
+++ src/app/exercice1/exercice1.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-exercice1',
   templateUrl: './exercice1.component.html',
   styleUrls: ['./exercice1.component.css']
 })
+
 export class Exercice1Component implements OnInit {
-  public compteur: number = 0; 
+  public result: number = 0; 
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  updateResult(event){
+    this.result = event.value;
   }
 
 }
